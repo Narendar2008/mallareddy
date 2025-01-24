6afdb8b8531c1f2248f45326ad7040120e00cc2a
Narendar2008 <138804053+Narendar2008@users.noreply.github.com>
2025-01-24 15:17:14 +0530

    Create binarySearchInsertion.java

---
+++ binarySearchInsertion.java
@@ -0,0 +1,11 @@
+if(root==null){
+    return new Node(data);
+   }
+   if(data<root.data){
+    root.left=insert(root.left,data);
+   }else{
+    root.right=insert(root.right,data);
+   }
+   return root;
+    	
+    }
