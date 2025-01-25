bd22e704b97846eaea7dee9a4869e93183639c90
Narendar2008 <138804053+Narendar2008@users.noreply.github.com>
2025-01-25 10:57:01 +0530

    Create BalancedBrackets.java

---
+++ BalancedBrackets.java
@@ -0,0 +1,30 @@
+ public static String isBalanced(String s) {
+Stack<Character> stack = new Stack<>();
+
+
+    for (char c : s.toCharArray()) {
+        // Push opening brackets onto the stack
+        if (c == '(' || c == '{' || c == '[') {
+            stack.push(c);
+        } 
+        
+        else if (c == ')' || c == '}' || c == ']') {
+            
+            if (stack.isEmpty()) {
+                return "NO";
+            }
+            char top = stack.pop();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return "NO";
+            }
+        }
+    }
+
+
+    return stack.isEmpty() ? "YES" : "NO";
+
+    }
+
+}
